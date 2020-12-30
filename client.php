ea2ea4c59a0a134b1af6528565c7dbc77b7d898e
FusionDev <unconfigured@null.spigotmc.org>
2020-12-30 20:03:36 +0100

    First commit

---
+++ client.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Client</title>
+
+    
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script src="//cdn.jsdelivr.net/npm/phaser@3.24.1/dist/phaser.min.js"></script>
+
+    <script>window.authTicket = 'fusion';</script>
+    
+    <link rel="stylesheet" href="client/assets/styles/loader.css">
+    <link rel="stylesheet" href="client/assets/styles/hotel_view.css">
+    <link rel="stylesheet" href="client/assets/styles/interface.css">
+    
+    <script src="client/src/pixel.js" type="module"></script>
+    </head>
+
+    <body>
+    </body>
+
+</html>
